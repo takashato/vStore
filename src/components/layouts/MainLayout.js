a4c33951e1204f50389d88acd4f40d9fa2f24b45
Bành Thanh Sơn <takashato@gmail.com>
2019-12-02 22:44:34 +0700

    Split SiderBar component

--- src/components/layouts/MainLayout.js
+++ src/components/layouts/MainLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout, Menu, Icon } from 'antd';
 import './MainLayout.css';
+import SideBar from './SideBar';
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -8,36 +9,7 @@ class MainLayout extends React.Component {
     render() {
         return (
             <Layout className='main-layout'>
-                <Sider
-                    breakpoint="lg"
-                    collapsedWidth="0"
-                    onBreakpoint={broken => {
-                        console.log(broken);
-                    }}
-                    onCollapse={(collapsed, type) => {
-                        console.log(collapsed, type);
-                    }}
-                >
-                    <div className="logo" />
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                        <Menu.Item key="1">
-                            <Icon type="user" />
-                            <span className="nav-text">nav 1</span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="video-camera" />
-                            <span className="nav-text">nav 2</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Icon type="upload" />
-                            <span className="nav-text">nav 3</span>
-                        </Menu.Item>
-                        <Menu.Item key="4">
-                            <Icon type="user" />
-                            <span className="nav-text">nav 4</span>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
+                <SideBar/>
                 <Layout>
                     <Header style={{ background: '#fff', padding: 0 }} />
                     <Content style={{ margin: '24px 16px 0' }}>
---
+++ src/components/layouts/SideBar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Icon, Menu, Layout} from "antd";
+
+const {Sider} = Layout;
+
+class SideBar extends React.Component {
+    render() {
+        return (
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={broken => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
+            >
+                <div className="logo" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                    <Menu.Item key="1">
+                        <Icon type="user" />
+                        <span className="nav-text">nav 1</span>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Icon type="video-camera" />
+                        <span className="nav-text">nav 2</span>
+                    </Menu.Item>
+                    <Menu.Item key="3">
+                        <Icon type="upload" />
+                        <span className="nav-text">nav 3</span>
+                    </Menu.Item>
+                    <Menu.Item key="4">
+                        <Icon type="user" />
+                        <span className="nav-text">nav 4</span>
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+        );
+    }
+}
+
+export default SideBar;
