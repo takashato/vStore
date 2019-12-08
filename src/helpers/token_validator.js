ea41db73575f9165b78a177a2419b1214e5213a4
Bành Thanh Sơn <takashato@gmail.com>
2019-12-08 21:23:38 +0700

    Add allowed groups to routes

--- src/helpers/token_validator.js
+++ src/helpers/token_validator.js
@@ -2,13 +2,22 @@ import Staff from '../models/staff_imported';
 
 const validate = async function (decoded, request, h) {
     // do your checks to see if the person is valid
-    let staff = Staff.findOne(decoded.id);
+    let staff = null;
+    try {
+        staff = await Staff.findByPk(decoded.id);
+    } catch (err) {
+        console.log(err);
+        return {isValid: false};
+    }
     if (!staff) {
-        return { isValid: false };
+        return {isValid: false};
     }
-    else {
-        return { isValid: true };
+    request.staff = staff;
+    if (request.route.settings.app.allowedGroups && !request.route.settings.app.allowedGroups.includes(staff.group_id)) { // Require group check
+        const response = h.response({statusCode: 403, userMessage: 'Bạn không được phép truy cập vào nội dung này.'}).code(403);
+        return {isValid: false, response};
     }
+    return {isValid: true};
 };
 
 export default validate;
--- src/routes/staff.js
+++ src/routes/staff.js
@@ -1,4 +1,4 @@
-import {authenticate} from "../handlers/staff";
+import {authenticate, getAllStaff} from "../handlers/staff";
 
 function applyRoute(server) {
     server.route({
@@ -7,6 +7,18 @@ function applyRoute(server) {
         handler: authenticate,
         options: {
             auth: false,
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/staff',
+        handler: getAllStaff,
+        options: {
+            auth: 'jwt',
+            app: {
+                allowedGroups: [1] // Admin
+            },
         }
     });
 }
