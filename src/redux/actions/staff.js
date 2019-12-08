ed12cdfe792ec112daa934e9ba3fb546f04f5e3f
Bành Thanh Sơn <takashato@gmail.com>
2019-12-08 19:34:28 +0700

    Set token on setToken action

--- src/redux/actions/staff.js
+++ src/redux/actions/staff.js
@@ -1,6 +1,10 @@
+import axios from "../../libs/axios";
+
 export const TOKEN_UPDATED = 'TOKEN_UPDATED';
 
 export function setToken(token) {
+    console.log('Token updated');
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
     return {
         type: 'TOKEN_UPDATED',
         token: token,
