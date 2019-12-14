c79f7ab19799af408c06e8ac5e5cf8d113dee0fc
Bành Thanh Sơn <takashato@gmail.com>
2019-12-14 15:56:47 +0700

    Associates model Product to ProductCategory

--- src/models/product.js
+++ src/models/product.js
@@ -18,7 +18,11 @@ module.exports = function(sequelize, DataTypes) {
 		},
 		category_id: {
 			type: DataTypes.INTEGER(11),
-			allowNull: false
+			allowNull: false,
+			references: {
+				model: 'product_category',
+				key: 'id'
+			}
 		},
 		price: {
 			type: "DOUBLE",
--- src/models/product_imported.js
+++ src/models/product_imported.js
@@ -1,5 +1,8 @@
 import sequelize from "../db";
+import ProductCategory from "./product_category_imported";
 
 const Product = sequelize.import(__dirname + "/product");
+Product.belongsTo(ProductCategory, {foreignKey: 'category_id', as: 'category'});
+ProductCategory.hasMany(Product, {foreignKey: 'category_id'});
 
 export default Product;
