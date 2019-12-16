96e0d11627c22a6dbce9acd237fbe2bd31061af5
Bành Thanh Sơn <takashato@gmail.com>
2019-12-16 17:15:47 +0700

    Update invoice_detail.sql

--- sql/invoice_detail.sql
+++ sql/invoice_detail.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100406
  File Encoding         : 65001
 
- Date: 10/12/2019 12:46:38
+ Date: 16/12/2019 17:15:18
 */
 
 SET NAMES utf8mb4;
@@ -23,13 +23,18 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `invoice_detail`;
 CREATE TABLE `invoice_detail`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `invoice_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `price` double NULL DEFAULT NULL,
   `quantity` double NULL DEFAULT NULL,
   `total_value` double NULL DEFAULT NULL,
   `discount_value` double NULL DEFAULT NULL,
   `total_final_value` double NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `created_at` datetime(0) NULL DEFAULT NULL,
+  `updated_at` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `invoice_id`(`invoice_id`) USING BTREE,
+  CONSTRAINT `invoice_detail_ibfk_1` FOREIGN KEY (`invoice_id`) REFERENCES `invoice` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
